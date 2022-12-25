a4c3ffa413d00b4fbc881db44ce3f8fd32b74c42
Kate <tsvetkova.e.i@mail.ru>
2022-12-25 14:27:52 +0300

    25/11

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ExampleSQLAdd
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Answer = new System.Windows.Forms.ListBox();
+            this.WordBox = new System.Windows.Forms.ListBox();
+            this.Generate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(129)))), ((int)(((byte)(218)))));
+            this.panel1.Controls.Add(this.Answer);
+            this.panel1.Controls.Add(this.WordBox);
+            this.panel1.Controls.Add(this.Generate);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(650, 452);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // Answer
+            // 
+            this.Answer.FormattingEnabled = true;
+            this.Answer.Location = new System.Drawing.Point(374, 300);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(246, 82);
+            this.Answer.TabIndex = 8;
+            this.Answer.SelectedIndexChanged += new System.EventHandler(this.Answer_SelectedIndexChanged);
+            // 
+            // WordBox
+            // 
+            this.WordBox.FormattingEnabled = true;
+            this.WordBox.Location = new System.Drawing.Point(24, 90);
+            this.WordBox.Name = "WordBox";
+            this.WordBox.Size = new System.Drawing.Size(154, 199);
+            this.WordBox.TabIndex = 7;
+            this.WordBox.SelectedIndexChanged += new System.EventHandler(this.WordBox_SelectedIndexChanged);
+            // 
+            // Generate
+            // 
+            this.Generate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Generate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(163)))), ((int)(((byte)(68)))));
+            this.Generate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Generate.FlatAppearance.BorderSize = 0;
+            this.Generate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(47)))), ((int)(((byte)(29)))));
+            this.Generate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(78)))), ((int)(((byte)(18)))));
+            this.Generate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Generate.ForeColor = System.Drawing.SystemColors.Window;
+            this.Generate.Location = new System.Drawing.Point(402, 122);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(125, 44);
+            this.Generate.TabIndex = 6;
+            this.Generate.Text = "Ganerate";
+            this.Generate.UseVisualStyleBackColor = false;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ExampleSQLAdd.Properties.Resources._lock;
+            this.pictureBox1.Location = new System.Drawing.Point(402, 230);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ExampleSQLAdd.Properties.Resources.user;
+            this.pictureBox2.Location = new System.Drawing.Point(285, 90);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(159)))), ((int)(((byte)(189)))));
+            this.panel2.Controls.Add(this.closeButton);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(650, 84);
+            this.panel2.TabIndex = 0;
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.closeButton.Location = new System.Drawing.Point(620, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(30, 29);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "X";
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.closeButton.MouseEnter += new System.EventHandler(this.closeButton_MouseEnter);
+            this.closeButton.MouseLeave += new System.EventHandler(this.closeButton_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(234)))), ((int)(((byte)(237)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(650, 84);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find Word with \"!\"";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 452);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label closeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.ListBox WordBox;
+        private System.Windows.Forms.ListBox Answer;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace ExampleSQLAdd
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_MouseEnter(object sender, EventArgs e)
+        {
+            closeButton.ForeColor = Color.Green;
+        }
+
+        private void closeButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.ForeColor = Color.White;
+        }
+        Point lastPoint;
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        }
+        
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            // Получаем количество слов и букв за слово.
+            int num_letters = 10;
+            int num_words = 10;
+            //WordBox.Items.Add($"string");
+            // Создаем массив букв, которые мы будем использовать.
+            char[] letters = "AB!CDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            //char[] letters = "ABCDEFGHIJKL!MNOPQRSTUVWXYZ".ToCharArray();
+            Create_words(10,10, letters);
+            //Console.WriteLine($"new_letters, new_words");
+        }
+        int answer=0;
+        //char [] v = "!".ToCharArray();
+        // Делаем слова.
+        public void Create_words(int num_letters,int num_words,char[] letters)
+        {
+            // Создаем генератор случайных чисел.
+            Random rand = new Random();
+            for (int i = 1; i < num_words; i++)
+            {
+                // Сделайте слово.
+                string word = "";
+                for (int j = 1; j < num_letters; j++)
+                {
+                    // Выбор случайного числа от 0 до 25
+                    // для выбора буквы из массива букв.
+                    int letter_num = rand.Next(0, letters.Length - 1); 
+
+                    // Добавить письмо.
+                    word += letters[letter_num];
+                }
+                // Добавьте слово в список.
+                WordBox.Items.Add( word);
+                //bool Contains(char value, StringComparison comparisonType);
+                string s2 = "!";
+                bool b = word.Contains(s2);
+                if(b)
+                {
+                    answer += 1;
+                }
+                //Answer.Items.Add(answer);
+            }
+            Answer.Items.Add(answer);
+            Console.WriteLine();
+        }
+        
+        
+        private void WordBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void Answer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Answer.Items.Add($"ansver");
+        }
+    }
+}
